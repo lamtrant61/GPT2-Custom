3481ad6ed3706d502c7f1aaf4a2f771f9f65c8dc
lamtt <lamtrant61@gmail.com>
2024-10-17 15:27:47 +0700

    add utils

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,7 +1,12 @@
 # utils/common.py
-import json
 
 def load_config(filename):
+    import json
     with open(filename, 'r', encoding='utf-8') as file:
         config = json.load(file)
     return config
+
+def load_csv_data(filename):
+    import pandas as pd
+    data = pd.read_excel(filename)
+    return data
