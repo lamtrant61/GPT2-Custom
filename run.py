e9599b28412ddc6d8b505b196c75f1e5ee9c9296
lamtt <lamtrant61@gmail.com>
2024-10-17 15:27:56 +0700

    add run file

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from src.model import run_model
+import os
+from src.model import Model_GPT2, run_model
+from src.utils.common import load_csv_data
 
-config = run_model("./src/config/config.json")
+config = run_model(os.path.abspath("./src/config/config.json"))
 model_generate_config = config["model_generate_config"]
+
+# data = load_csv_data(os.path.abspath("./data/data_test.xlsx"))
+# print(data)
+
+Model = Model_GPT2()
+dataset = Model.loading_dataset(os.path.abspath("./data/data_test.xlsx"))
+print (dataset)
