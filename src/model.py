dd54b5f3057a2347ca169ab6e1832c006263bee5
lamtt <lamtrant61@gmail.com>
2024-10-18 09:34:30 +0700

    update model

--- src/model.py
+++ src/model.py
@@ -1,31 +1,37 @@
 import os
 from dotenv import load_dotenv
-from transformers import TFGPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
-from .utils.common import load_config, load_csv_data
-import tensorflow as tf
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+from .utils.common import load_config, load_csv_data, get_device
+from .utils.dataset import TextDataset
 from tqdm import tqdm
-from datasets import Dataset
+# from datasets import Dataset
 import pandas as pd
+
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"  # log all model checkpoints
+os.environ["WANDB_MODE"] = "dryrun"  # offline mode
 
 load_dotenv()
 model_name = os.getenv('MODEL_NAME')
 
 print(model_name)
+
 class Model_GPT2:
     def __init__(self, model_name=model_name):
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         self.tokenizer.pad_token = self.tokenizer.eos_token  # Đặt pad token là eos token
-        self.model = TFGPT2LMHeadModel.from_pretrained(model_name, from_pt=True)
-        self.model.summary()
+        self.model = GPT2LMHeadModel.from_pretrained(model_name)
+        self.model.resize_token_embeddings(len(self.tokenizer))
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def loading_dataset(self, filename):
         df = load_csv_data(filename)
         df['text'] = df['Question'] + " " + df['Answer']
 
-        # Apply tokenizer to all text
-        tokenized_data = df['text'].apply(self.preprocess_data)
-        tokenized_df = pd.DataFrame(tokenized_data.tolist())
-        return Dataset.from_pandas(tokenized_df)
+        texts = df['text'].tolist() 
+        encodings = self.tokenizer(texts, truncation=True, padding=True, return_tensors='pt', max_length=512)
+        dataset = TextDataset(encodings)
+        return dataset
 
     def preprocess_data(self, data):
         return self.tokenizer(data, padding="max_length", truncation=True, max_length=512)
@@ -36,16 +42,17 @@ class Model_GPT2:
         output = self.model.generate(input_ids, max_length=100, num_return_sequences=1)
         return self.tokenizer.decode(output[0], skip_special_tokens=True)
 
-    def train(self, data, lr=5e-5, epochs=3):
+    def train(self, train_dataset, lr=5e-5, epochs=3):
         training_args = TrainingArguments(
-            output_dir='../results',          # output directory
-            num_train_epochs=epochs,         # number of training epochs
-            per_device_train_batch_size=4,   # batch size for training
-            warmup_steps=500,                # number of warmup steps for learning rate scheduler
-            weight_decay=0.01,               # strength of weight decay
-            logging_dir='../logs',            # directory for storing logs
-            logging_steps=10,
-            learning_rate=lr
+            output_dir='./results',          # output directory
+            report_to="wandb",            # directory for storing logs
+            run_name="wandb_chat_gpt2",  # name of the W&B run (optional)
+            logging_steps=1,  # how often to log to W&B
+            num_train_epochs=20,               # Total number of training epochs
+            per_device_train_batch_size=4,    # Batch size per device during training
+            save_steps=10_000,                 # After how many steps to save the model
+            save_total_limit=2,                # Limit the total amount of checkpoints
+            prediction_loss_only=True,
         )
 
         trainer = Trainer(
@@ -55,7 +62,7 @@ class Model_GPT2:
         )
         trainer.train()
 
-    def save(self, path='../models'):
+    def save(self, path='./models'):
         self.model.save_pretrained(path)
         self.tokenizer.save_pretrained(path)
         
