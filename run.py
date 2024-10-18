8a8593df9aa946d39e3fd466118960c2f2860f38
lamtt <lamtrant61@gmail.com>
2024-10-18 09:35:03 +0700

    update run file

--- run.py
+++ run.py
@@ -5,9 +5,8 @@ from src.utils.common import load_csv_data
 config = run_model(os.path.abspath("./src/config/config.json"))
 model_generate_config = config["model_generate_config"]
 
-# data = load_csv_data(os.path.abspath("./data/data_test.xlsx"))
-# print(data)
-
 Model = Model_GPT2()
 dataset = Model.loading_dataset(os.path.abspath("./data/data_test.xlsx"))
-print (dataset)
+
+Model.train(dataset)
+Model.save()
