7a8311c639449a0ca301b687344f3d0f5cd3f919
lamtt <lamtrant61@gmail.com>
2024-10-18 16:12:07 +0700

    clean question text for generate

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,14 +1,24 @@
 from torch.utils.data import Dataset
 
 class TextDataset(Dataset):
-    def __init__(self, encodings):
+    def __init__(self, encodings, sep_token_id):
         self.encodings = encodings
+        self.sep_token_id = sep_token_id
 
     def __len__(self):
         return len(self.encodings['input_ids'])
 
     def __getitem__(self, idx):
-        # Set labels as the input_ids for language modeling
+        # Lấy input_ids, attention_mask và chuẩn bị labels
         item = {key: val[idx] for key, val in self.encodings.items()}
-        item['labels'] = item['input_ids']  # Set labels for loss calculation
+
+        # Tìm vị trí của token <|sep|> để tách câu hỏi khỏi câu trả lời
+        sep_index = (item['input_ids'] == self.sep_token_id).nonzero(as_tuple=True)[0].item()
+
+        # Gán labels sao cho phần câu hỏi có giá trị -100 (để bỏ qua khi tính loss)
+        labels = item['input_ids'].clone()
+        labels[:sep_index + 1] = -100  # Bỏ qua phần câu hỏi + token <|sep|>
+
+        item['labels'] = labels  # Gán lại labels
+
         return item
