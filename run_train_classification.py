9ec9fcff33898cfb9b82a322529be37f6c977bb0
lamtt <lamtrant61@gmail.com>
2024-10-21 11:56:23 +0700

    update run model class

--- run_train_classification.py
+++ run_train_classification.py
@@ -2,7 +2,6 @@ import os
 from src.model_classification import Model_SVC
 
 Model = Model_SVC()
-dataset = Model.load_data_train(os.path.abspath("./data/classification_data.xlsx"))
-
-Model.train(dataset)
-Model.save()
+Model.load_data_train(os.path.abspath("./data/classification_data.xlsx"))
+Model.train()
+Model.dump_model()
